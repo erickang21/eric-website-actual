de3576ff4ec3ad12b35bd6fe9dde906410c23118
Eric Kang <kang.eric.hi@gmail.com>
2024-04-28 15:08:52 -0400

    add heading

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../components/Header";
+import "../css/pages/Home.css";
+
+const Home = (): React.ReactElement => {
+    return (
+        <div className="home">
+            <Header />
+        </div>
+    )
+}
+
+export default Home;
